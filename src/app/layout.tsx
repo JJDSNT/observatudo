70c24a9dfc85d6fcb9849d1a7664586c7b305ed6
Jaime Dias <jaimejosediasnt@gmail.com>
2023-10-05 15:11:04 -0300

    ajustando metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,24 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'ObservaTudo',
   description: 'Dashboard de indicadores cívicos',
+  themeColor: '#041E42',
+  twitter: {
+    card: "summary",
+    title:"ObservaTudo",
+    description:"ObservaTudo - Dashboard de indicadores cívicos",
+    images: "https://www.observatudo.com.br/android-chrome-512x512.png"
+  },
+  openGraph: {
+    type: "website",
+    url: "https://observatudo.com.br/",
+    title: "ObservaTudo",
+    description: "ObservaTudo - Dashboard de indicadores cívicos",
+    siteName: "Observa Tudo",
+    images: [{
+      url: "https://www.observatudo.com.br/android-chrome-512x512.png",
+    }],
+  }
+
 }
 
 export default function RootLayout({
@@ -15,7 +33,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <head />
       <body className={inter.className}>{children}</body>
     </html>
   )
