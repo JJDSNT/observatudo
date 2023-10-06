763dafa171666de9aa1f96d8d38c10bb2aaa23ab
Jaime Dias <jaimejosediasnt@gmail.com>
2023-10-06 04:50:46 -0300

    quick fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import Layout from '@/app/Layout/Layout';
 const Home = () => {
   return (
     <div className="container mx-auto p-4">
-      if (typeof window !== 'undefined') {
-        <Layout />
-      }
+      {typeof window !== 'undefined' && <Layout />}
     </div>
   );
 };
