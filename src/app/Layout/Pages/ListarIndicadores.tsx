cd3ae73e79278413d9dd3d56aeff9359d3448efc
Jaime Dias <jaimejosediasnt@gmail.com>
2023-10-06 20:42:07 -0300

    listar indicadores (inicial)

--- src/app/Layout/Pages/ListarIndicadores.tsx
+++ src/app/Layout/Pages/ListarIndicadores.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import httpClient from '@/app/utils/httpClient';
 
 function ListarIndicadores() {
+  const [eixos, setEixos] = useState([]);
+
+  useEffect(() => {
+    const fetchIndicadores = async () => {
+      try {
+        const response = await httpClient.get('/api/indicadores/eixo');
+        const data = response.data;
+        console.log('resultado ' + data);
+
+        if (data && data.eixos) {
+          setEixos(data.eixos);
+        }
+      } catch (error) {
+        console.error('Ocorreu um erro ao buscar os indicadores:', error);
+      }
+    };
+
+    fetchIndicadores();
+  }, []);
+
   return (
     <div>
-      {/* Coloque o conteúdo da página "Listar indicadores" aqui */}
-      <h2>Listar indicadores</h2>
-      {/* Adicione o conteúdo da página aqui */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {eixos.map((eixo) => (
+          <div key={eixo.id} className={`p-4 ${eixo.cor}`}>
+            <h2 className="text-2xl font-bold mb-2">{eixo.nome}</h2>
+            {eixo.indicadores.map((indicador) => (
+              <div key={indicador.codigo_indicador} className="mb-2">
+                <h3 className="text-lg font-semibold">{indicador.nome}</h3>
+                <p className="text-sm">{indicador.descricao}</p>
+                <p className="text-sm">{indicador.codigo_indicador}</p>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
