dba80f0e17c6ba185b006549bc332797a48313c5
Jaime Dias <jaimejosediasnt@gmail.com>
2023-10-12 18:37:25 -0300

    outra opção para passar os dados

--- src/app/controllers/data/ParseValoresController.tsx
+++ src/app/controllers/data/ParseValoresController.tsx
@@ -17,7 +17,53 @@ export async function populateValoresIndicador(): Promise<void> {
     const bucketName = 'raw-sources';
     const batchSize = 100;
 
-    const indicadoresDesejados = ["3982", "5155", "27"];//vazio processará todos
+ // colocar uma mensagem de gravação concluida quando finalizar a gravação   
+    
+    
+    const indicatorsToUpdate = [
+        // Saúde (Health)
+    
+        { codigo_indicador: '4009', eixoId: 1 },
+        { codigo_indicador: '125', eixoId: 1 },
+//        { codigo_indicador: '3982', eixoId: 1 },
+        { codigo_indicador: '4008', eixoId: 1 },
+    
+        // Educação (Education)
+        { codigo_indicador: '4016', eixoId: 2 },
+        { codigo_indicador: '4148', eixoId: 2 },
+  //      { codigo_indicador: '5155', eixoId: 2 },
+    //    { codigo_indicador: '27', eixoId: 2 },
+        { codigo_indicador: '4022', eixoId: 2 },
+    
+        // Assistência Social (Social Assistance)
+        { codigo_indicador: '3965', eixoId: 3 },
+        { codigo_indicador: '3985', eixoId: 3 },
+    
+        // Segurança (Security)
+        { codigo_indicador: '3900', eixoId: 4 },
+    
+        // Meio ambiente, urbanização e mobilidade (Environment, Urbanization, and Mobility)
+        { codigo_indicador: '127', eixoId: 5 },
+        { codigo_indicador: '5140', eixoId: 5 },
+        { codigo_indicador: '3865', eixoId: 5 },
+    
+        // Economia & Finanças (Economy & Finance)
+        { codigo_indicador: '23', eixoId: 6 },
+    
+        // Governança & Administração (Governança)
+        { codigo_indicador: '4120', eixoId: 7 },
+        { codigo_indicador: '3922', eixoId: 7 },
+        { codigo_indicador: '4005', eixoId: 7 },
+    ];
+    
+    
+    
+    
+    
+    const indicadoresDesejados = indicatorsToUpdate.map(indicador => indicador.codigo_indicador);
+
+    
+    //const indicadoresDesejados = ["3982", "5155", "27"];//vazio processará todos
 
 
     const jsonDataIndicadores = await storageService.parseCSVtoJSON(bucketName, 'cidades_sustentaveis/indicadores.csv');
