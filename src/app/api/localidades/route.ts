1845d27cc38fc349a32020b60684d5589f65e3f7
Jaime Dias <jaimejosediasnt@gmail.com>
2023-10-26 01:58:03 -0300

    um rascunho inicial?

--- src/app/api/localidades/route.ts
+++ src/app/api/localidades/route.ts
@@ -1,10 +1,29 @@
-import { Container } from 'typedi';
-import { LocalidadeController } from "@/app/controllers/LocalidadeController";
+import { AppDataSource, initializeDatabase } from '@/app/infra/database';
+import { Estado } from '@/app/models/Estado';
 
 export async function GET() {
+
+  initializeDatabase()//passar parametro de onde esta sendo chamado para log do erro
+
   try {
-    const localidadeController = Container.get(LocalidadeController);
-    const estados = await localidadeController.getEstadosECidades();
+    const estados = await AppDataSource.manager.getRepository(Estado).find({
+      relations: ["capital", "cidades"]
+    });
+
+    if (estados) {
+      estados.forEach(estado => {
+        // Ordena as cidades em ordem alfabética pelo nome
+        if (estado.cidades) {
+          estado.cidades.sort((a, b) => a.nome.localeCompare(b.nome));
+        }
+        // Encontre a capital do estado selecionado
+        //const capitalCidade = estado.capital;
+        // Reorganize a lista de cidades para começar com a capital do estado
+        //estado.cidades = [capitalCidade, ...estado.cidades.filter(cidade => cidade !== capitalCidade)];
+      });
+      // Ordena os estados em ordem alfabética pelo nome
+      estados.sort((a, b) => a.nome.localeCompare(b.nome));
+    }
     return Response.json({ estados });
   } catch (error: unknown) {
     if (error instanceof Error) {
